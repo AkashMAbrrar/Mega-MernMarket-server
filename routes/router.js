5863bc4081dd113396dd91fac9e216cdab45b3a6
AkashAbrrar <mabrrar9@gmail.com>
2022-08-05 22:58:49 +0600

    this commit for commit port secret

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { userSignup } from '../controller/user-controller.js';
-
+import { userSignup, userLogin } from '../controller/user-controller.js';
+import { getProducts, getProductById } from '../controller/product-controller.js';
 const router = express.Router();
 
 router.post('/signup', userSignup);
+router.post('/login', userLogin);
+
+// get products router
+router.get('/products', getProducts);
+router.get('/product/:id', getProductById)
 
 export default router;
